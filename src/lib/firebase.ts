4c0ea08b87f16b5eab85fdc98faef18948e42ab6
sahind01 <164308037+sahind01@users.noreply.github.com>
2026-06-19 19:40:17 +0300

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,24 @@
+// src/lib/firebase.ts
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4GZiQKkqyu6fnJDAwlFb4Siy5_FnzuxE",
+  authDomain: "sahink-5e99d.firebaseapp.com",
+  databaseURL: "https://sahink-5e99d-default-rtdb.firebaseio.com",
+  projectId: "sahink-5e99d",
+  storageBucket: "sahink-5e99d.firebasestorage.app",
+  messagingSenderId: "983830185039",
+  appId: "1:983830185039:web:02790369dedfb5d901163a",
+  measurementId: "G-FEX7NBQG0R"
+};
+
+// Firebase'i başlat
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
